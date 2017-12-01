1a6a254deb3ef8dd80d31214fb857ab900504bac
smashin <smashin@33slona.ru>
2017-12-01 03:05:32 +0300

    docker compose support with truffle

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,9 @@
 var Owned = artifacts.require("./Owned.sol");
-var NursingHomeDonates = artifacts.require("./Owned.sol");
+var NursingHomeDonates = artifacts.require("./NursingHomeDonates.sol");
 
 module.exports = function (deployer, network) {
-    if (network == 'development') {
+    console.log(network, ' - network')
+    if (network != 'stage') {
         deployer.deploy(Owned);
         deployer.deploy(NursingHomeDonates);
     }
--- truffle.js
+++ truffle.js
@@ -7,13 +7,13 @@ module.exports = {
             port: process.env.RPC_PORT || 8545,
             from: "0x6B0c56d1Ad5144b4d37fa6e27DC9afd5C2435c3B",
             gas: 47000000,
-            network_id: "*" // Match any network id
+            network_id: "8995" // Match any network id
         },
         stage: {
             host: "78.24.223.194",
             port: 8545,
             from: "0x6B0c56d1Ad5144b4d37fa6e27DC9afd5C2435c3B",
-            network_id: "*",
+            network_id: "8995",
             gas: 47000000,
             // optional config values:
             // gas
